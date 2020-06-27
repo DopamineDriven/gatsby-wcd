2f662a1203859f2a6ce22c8c023e0a7a10cd0ff3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-06-26 23:04:26 -0500

    building out gatsby starter site

---
+++ src/components/layout.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+import { rhythm } from "../utils/typography";
+
+const MainLayout: FC = ({ children }) => (
+	<div
+		style={{
+			margin: `0 auto`,
+			marginBottom: rhythm(1.5),
+			marginTop: rhythm(1.5),
+			maxWidth: 650,
+			paddingLeft: rhythm(3 / 4),
+			paddingRight: rhythm(3 / 4)
+		}}
+	>
+		{children}
+	</div>
+);
+
+export default MainLayout;
